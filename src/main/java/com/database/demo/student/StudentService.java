d2ec7d2695eb08b251c8cff8d32693cec70b3ca4
danielramirez <dannyramirez917@gmail.com>
2021-05-09 12:20:15 -0400

    update

--- src/main/java/com/database/demo/student/StudentService.java
+++ src/main/java/com/database/demo/student/StudentService.java
@@ -12,10 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- *
- * @author danielramirez
- */
 @Service
 public class StudentService {
 
